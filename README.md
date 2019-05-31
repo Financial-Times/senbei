4401b99cfb15b9d5e25b871819e0ddeb876346c9
Tak Tran <contact@tutaktran.com>
2019-05-31 14:33:17 +0100

    Add initial implementation of senbei

--- README.md
+++ README.md
@@ -1,6 +1,58 @@
-# senbei
+# 🍘 Senbei
 
-A command line tool to generate commands from simple templates
+Generate tasty command line snacks, using simple templates.
+
+Senbei (せんべい) is Japanese for [rice cracker](https://translate.google.com/#view=home&op=translate&sl=en&tl=ja&text=rice%20cracker).
+
+```shell
+$ export SENBEI_TEMPLATES="/some/path/to/senbei-templates"
+$ senbei
+✔ Choose a template · echo.js
+? Fill out the fields in the following command › 0% completed
+ echo "<something here>"
+
+# After typing something for `<something here>`
+
+✔ Fill out the fields in the following command · 100% completed
+✔ Do you want to run
+
+echo "something"
+
+? (Y/n) · true
+something
+```
+
+Built on top of [enquirer](https://www.npmjs.com/package/enquirer).
+
+## Usage
+
+* Install `senbei`
+
+      npm install -g senbei
+
+* Set the `SENBEI_TEMPLATES` environment variable to be the folder where templates are stored
+
+  The format of a template is
+
+    ```javascript
+    // echo.js
+
+    // Add modifiable fields with `\${fieldName}`
+    const command = `echo "\${something}"`;
+
+    // `fieldName` can be added here with a message
+    // If there are no modifiable fields, `fields` can be empty
+    const fields = [
+      {
+        name: "something",
+        message: "something here"
+      }
+    ];
+
+    module.exports = { command, fields };
+    ```
+
+* Run: `senbei`
 
 ## Development
 
@@ -12,10 +64,6 @@ A command line tool to generate commands from simple templates
 - Install dependencies
 
         npm install
-
-- To run the `*:watch` tasks install [nodemon](https://github.com/remy/nodemon) globally
-
-        npm install -g nodemon
 
 - To start the app
 
--- index.js
+++ index.js
@@ -1,1 +1,84 @@
-console.log('Hello hello');
+const { readdirSync, existsSync } = require('fs');
+const { execSync } = require('child_process');
+const { join } = require('path');
+const { prompt } = require('enquirer');
+
+const { SENBEI_TEMPLATES } = process.env;
+
+function templateFiles() {
+  if (!SENBEI_TEMPLATES) {
+    throw new Error(
+      'No template directory found. Set the `SENBEI_TEMPLATES` environment variable to the folder where senbei templates are stored.'
+    );
+  } else if (!existsSync(SENBEI_TEMPLATES)) {
+    throw new Error(
+      `${SENBEI_TEMPLATES} directory not found. Set the \`SENBEI_TEMPLATES\` environment variable to the folder where senbei templates are stored.`
+    );
+  }
+
+  return readdirSync(SENBEI_TEMPLATES);
+}
+
+function getSnippetPrompt({ command, fields }) {
+  const hasFields = !!fields;
+
+  return hasFields
+    ? {
+        type: 'snippet',
+        name: 'command',
+        message: 'Fill out the fields in the following command',
+        required: true,
+        fields,
+        template: command
+      }
+    : undefined;
+}
+
+async function main() {
+  const choices = templateFiles();
+  const selectTemplateFilePrompt = {
+    type: 'select',
+    name: 'templateFile',
+    message: 'Choose a template',
+    required: true,
+    choices
+  };
+
+  const { templateFile } = await prompt(selectTemplateFilePrompt);
+
+  try {
+    const file = join(SENBEI_TEMPLATES, templateFile);
+    // Hack: don't dynamically require files - find a better way
+    const fileContents = require(file);
+
+    let { command } = fileContents;
+    const snippetPrompt = getSnippetPrompt(fileContents);
+    const shouldRunPrompt = {
+      type: 'confirm',
+      name: 'shouldRun',
+      initial: true,
+      message: ({ answers: { command: { result } = {} } = {} }) => {
+        const hasCommand = !!result;
+        if (hasCommand) {
+          command = result;
+        }
+        return `Command: \n\n${command}\n\nRun it?`;
+      },
+      required: true
+    };
+    const prompts = !!snippetPrompt
+      ? [snippetPrompt, shouldRunPrompt]
+      : shouldRunPrompt;
+    const response = await prompt(prompts);
+    const { shouldRun } = response;
+
+    if (shouldRun) {
+      const out = execSync(command);
+      console.log(out.toString());
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main();
